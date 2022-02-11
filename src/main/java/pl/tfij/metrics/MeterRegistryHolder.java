68dd3c45bb967ac0941828b69b02258db257b55a
Tomasz Fijałkowski <tomasz@chi.pl>
2022-02-11 19:31:24 +0100

    Add lateInitMeterRegistry method

--- src/main/java/pl/tfij/metrics/MeterRegistryHolder.java
+++ src/main/java/pl/tfij/metrics/MeterRegistryHolder.java
@@ -3,12 +3,15 @@ package pl.tfij.metrics;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 import static java.util.Objects.requireNonNull;
 
 public class MeterRegistryHolder {
     private static MeterRegistry METER_REGISTRY = new SimpleMeterRegistry();
+    private static List<Consumer<MeterRegistry>> LATE_INIT_METRICS = new ArrayList<>();
     private static boolean isInitialized = false;
 
     private MeterRegistryHolder() {
@@ -21,14 +24,24 @@ public class MeterRegistryHolder {
         }
         METER_REGISTRY = requireNonNull(meterRegistry);
         isInitialized = true;
+        LATE_INIT_METRICS.forEach(consumer -> consumer.accept(METER_REGISTRY));
     }
 
     static public MeterRegistry meterRegistry() {
         return METER_REGISTRY;
     }
 
+    static public void lateInitMeterRegistry(Consumer<MeterRegistry> meterRegistryConsumer) {
+        if (isInitialized) {
+            meterRegistryConsumer.accept(METER_REGISTRY);
+        } else {
+            LATE_INIT_METRICS.add(meterRegistryConsumer);
+        }
+    }
+
     static void reset() {
         METER_REGISTRY = new SimpleMeterRegistry();
         isInitialized = false;
+        LATE_INIT_METRICS.clear();
     }
 }
--- src/test/java/pl/tfij/metrics/MeterRegistryHolderTest.java
+++ src/test/java/pl/tfij/metrics/MeterRegistryHolderTest.java
@@ -1,8 +1,12 @@
 package pl.tfij.metrics;
 
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.verification.VerificationMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -10,19 +14,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 class MeterRegistryHolderTest {
+    private final MeterRegistry meterRegistryMock = mock(MeterRegistry.class);
+
+    @BeforeEach
+    void setup() {
+        Mockito.when(meterRegistryMock.counter(anyString())).thenReturn(mock(Counter.class));
+    }
 
     @AfterEach
     void cleanup() {
+        meterRegistryMock.clear();
         MeterRegistryHolder.reset();
     }
 
     @Test
     void shouldReturnInitializedMeterRegistry() {
         // given initialized holder
-        MeterRegistry initMeterRegistry = mock(MeterRegistry.class);
+        MeterRegistry initMeterRegistry = meterRegistryMock;
         MeterRegistryHolder.init(initMeterRegistry);
 
         // when get meterRegistry from holder
@@ -47,7 +59,7 @@ class MeterRegistryHolderTest {
         MeterRegistryHolder.meterRegistry().counter("some.metric").increment();
 
         // and I initialized the holder
-        MeterRegistry initMeterRegistry = mock(MeterRegistry.class);
+        MeterRegistry initMeterRegistry = meterRegistryMock;
         MeterRegistryHolder.init(initMeterRegistry);
 
         // then metric before initialization is dropped
@@ -57,10 +69,40 @@ class MeterRegistryHolderTest {
     @Test
     void shouldThrowExceptionWhenTryToInitializeAlreadyInitializedHolder() {
         // given initialized holder
-        MeterRegistry initMeterRegistry = mock(MeterRegistry.class);
+        MeterRegistry initMeterRegistry = meterRegistryMock;
         MeterRegistryHolder.init(initMeterRegistry);
 
         // expect IllegalStateException when try to initialize holder again
         assertThrows(IllegalStateException.class, () -> MeterRegistryHolder.init(initMeterRegistry));
+    }
+
+    @Test
+    void shouldExecuteMetricConsumerOnInitializedHolder() {
+        // given initialized holder
+        MeterRegistry initMeterRegistry = meterRegistryMock;
+        MeterRegistryHolder.init(initMeterRegistry);
+
+        // when register a metric by lateInitMeterRegistry method
+        MeterRegistryHolder.lateInitMeterRegistry(meterRegistry -> meterRegistry.counter("some.metric").increment());
+
+        // then metric is registered on initMeterRegistry
+        verify(initMeterRegistry, exactlyOnce()).counter("some.metric");
+    }
+
+    @Test
+    void shouldExecuteMetricConsumerHandledBeforeInitialization() {
+        // when register a metric by lateInitMeterRegistry method
+        MeterRegistryHolder.lateInitMeterRegistry(meterRegistry -> meterRegistry.counter("some.metric").increment());
+
+        // and initialize the holder
+        MeterRegistry initMeterRegistry = meterRegistryMock;
+        MeterRegistryHolder.init(initMeterRegistry);
+
+        // then metric is registered on initMeterRegistry
+        verify(initMeterRegistry, exactlyOnce()).counter("some.metric");
+    }
+
+    private VerificationMode exactlyOnce() {
+        return times(1);
     }
 }
