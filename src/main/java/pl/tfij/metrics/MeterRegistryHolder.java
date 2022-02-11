9a6d3c8d877deef591a31dc14792482e5309b946
Tomasz Fijałkowski <tomasz@chi.pl>
2022-02-11 19:37:42 +0100

    make init and lateInitMeterRegistry thread safe

--- src/main/java/pl/tfij/metrics/MeterRegistryHolder.java
+++ src/main/java/pl/tfij/metrics/MeterRegistryHolder.java
@@ -5,25 +5,37 @@ import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
 
 import static java.util.Objects.requireNonNull;
 
 public class MeterRegistryHolder {
     private static MeterRegistry METER_REGISTRY = new SimpleMeterRegistry();
-    private static List<Consumer<MeterRegistry>> LATE_INIT_METRICS = new ArrayList<>();
-    private static boolean isInitialized = false;
+    private static final List<Consumer<MeterRegistry>> LATE_INIT_METRICS = new ArrayList<>();
+    private static boolean IS_INITIALIZED = false;
+    private static final Lock LOCK = new ReentrantLock();
 
     private MeterRegistryHolder() {
         throw new IllegalStateException("Can't create instance of util class");
     }
 
     static void init(MeterRegistry meterRegistry) {
-        if (isInitialized) {
+        LOCK.lock();
+        try {
+            synchronizeInit(meterRegistry);
+        } finally {
+            LOCK.unlock();
+        }
+    }
+
+    private static void synchronizeInit(MeterRegistry meterRegistry) {
+        if (IS_INITIALIZED) {
             throw new IllegalStateException("MeterRegistryHolder already initialized");
         }
         METER_REGISTRY = requireNonNull(meterRegistry);
-        isInitialized = true;
+        IS_INITIALIZED = true;
         LATE_INIT_METRICS.forEach(consumer -> consumer.accept(METER_REGISTRY));
     }
 
@@ -32,7 +44,16 @@ public class MeterRegistryHolder {
     }
 
     static public void lateInitMeterRegistry(Consumer<MeterRegistry> meterRegistryConsumer) {
-        if (isInitialized) {
+        LOCK.lock();
+        try {
+            synchronizeLateInitMeterRegistry(meterRegistryConsumer);
+        } finally {
+            LOCK.unlock();
+        }
+    }
+
+    private static void synchronizeLateInitMeterRegistry(Consumer<MeterRegistry> meterRegistryConsumer) {
+        if (IS_INITIALIZED) {
             meterRegistryConsumer.accept(METER_REGISTRY);
         } else {
             LATE_INIT_METRICS.add(meterRegistryConsumer);
@@ -41,7 +62,7 @@ public class MeterRegistryHolder {
 
     static void reset() {
         METER_REGISTRY = new SimpleMeterRegistry();
-        isInitialized = false;
+        IS_INITIALIZED = false;
         LATE_INIT_METRICS.clear();
     }
 }
